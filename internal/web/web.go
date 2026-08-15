ced412a6a6b7a2be4a0680ef8d0bd995911fc1ba
Aashay Tambi <aashay2035@gmail.com>
2026-08-15 13:32:59 -0700

    Fix login session cookie on HTTP and Ingress, set X-Frame-Options to SAMEORIGIN, bump to 0.2.2

--- internal/web/web.go
+++ internal/web/web.go
@@ -112,7 +112,7 @@ func SecurityHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		h := w.Header()
 		h.Set("X-Content-Type-Options", "nosniff")
-		h.Set("X-Frame-Options", "DENY")
+		h.Set("X-Frame-Options", "SAMEORIGIN")
 		h.Set("Referrer-Policy", "same-origin")
 		next.ServeHTTP(w, r)
 	})
